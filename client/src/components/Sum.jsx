48ef3afe2053132331e12f32adc14a36adea9454
arturterbaghdasarov1997 <133400776+arturterbaghdasarov1997@users.noreply.github.com>
2024-07-30 16:21:00 +0400

    Update Sum.jsx

--- client/src/components/Sum.jsx
+++ client/src/components/Sum.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-const Sum = ({ coffee, selectedIngredient, totalPrice }) => {
+const Sum = ({ coffee, ingredient }) => {
+  const coffeePrice = coffee ? parseFloat(coffee.price) : 2;
+  const ingredientPrice = ingredient ? parseFloat(ingredient.price) : 0;
+  const totalPrice = coffeePrice + ingredientPrice;
+
   return (
     <div className="totalPrice">
       <h2>Total Price</h2>
-      {coffee && <p><strong>Coffee:</strong> {coffee.title} - ₾{coffee.price}</p>}
-      {selectedIngredient && <p><strong>Ingredient:</strong> {selectedIngredient.name} - ₾{selectedIngredient.price}</p>}
-      <h3>₾{totalPrice}</h3>
+      {coffee && (
+        <p>
+          <strong>Coffee:</strong> {coffee.title} - ₾{coffeePrice.toFixed(2)}
+        </p>
+      )}
+      {ingredient && (
+        <p>
+          <strong>Ingredient:</strong> {ingredient.name} - ₾{ingredientPrice}
+        </p>
+      )}
+      <h3>Total: ₾{totalPrice.toFixed}</h3>
     </div>
   );
 };
