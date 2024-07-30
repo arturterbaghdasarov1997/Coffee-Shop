8008a72bbef6cc8e5d6fadb7587d17eea5baec8e
arturterbaghdasarov1997 <133400776+arturterbaghdasarov1997@users.noreply.github.com>
2024-07-30 16:22:03 +0400

    Update ClientPage.jsx

--- client/src/pages/ClientPage.jsx
+++ client/src/pages/ClientPage.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import CoffeeSelection from '../components/CoffeeSelection';
 import IngredientSelection from '../components/IngredientSelection';
-import Sum from '../components/Sum';
 import { useClientCoffeeContext } from '../context/ClientCoffeeContext';
 import '../App.css';
 
 const ClientPage = () => {
   const { coffees, ingredients, selectedCoffee, selectedIngredient, handleSelectCoffee, handleToggleIngredient } = useClientCoffeeContext();
-
-  const calculatePrice = () => {
-    const coffeePrice = selectedCoffee ? parseFloat(selectedCoffee.price) : 0;
-    const ingredientPrice = selectedIngredient ? parseFloat(selectedIngredient.price) : 0;
-
-    console.log('Coffee Price:', coffeePrice);
-    console.log('Ingredient Price:', ingredientPrice);
-
-    return (isNaN(coffeePrice) ? 2 : coffeePrice) + (isNaN(ingredientPrice));
-  };
 
   return (
     <div className="container">
@@ -32,9 +21,6 @@ const ClientPage = () => {
           selectedIngredient={selectedIngredient}
           onToggleIngredient={handleToggleIngredient}
         />
-      )}
-      {selectedCoffee && (
-        <Sum totalPrice={calculatePrice()} />
       )}
     </div>
   );
