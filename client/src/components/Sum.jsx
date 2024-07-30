d31454f0d56112acd8335f0b0c818f7136cdc2b8
arturterbaghdasarov1997 <133400776+arturterbaghdasarov1997@users.noreply.github.com>
2024-07-30 15:09:09 +0400

    Update Sum.jsx

--- client/src/components/Sum.jsx
+++ client/src/components/Sum.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-const Sum = ({ coffee, ingredients, totalPrice }) => {
-    return (
-        <div className="totalPrice">
-            <h2>Total Price</h2>
-            {coffee && <p><strong>Coffee:</strong> {coffee.name} - ₾{coffee.price}</p>}
-            <ul>
-                {ingredients.map((ingredient) => (
-                    <li key={ingredient.id}>{ingredient.name} - ₾{ingredient.price}</li>
-                ))}
-            </ul>
-            <h3>Total Price: ₾{totalPrice.toFixed(2)}</h3>
-        </div>
-    );
+const Sum = ({ coffee, selectedIngredient, totalPrice }) => {
+  return (
+    <div className="totalPrice">
+      <h2>Total Price</h2>
+      {coffee && <p><strong>Coffee:</strong> {coffee.title} - ₾{coffee.price}</p>}
+      {selectedIngredient && <p><strong>Ingredient:</strong> {selectedIngredient.name} - ₾{selectedIngredient.price}</p>}
+      <h3>₾{totalPrice}</h3>
+    </div>
+  );
 };
 
 export default Sum;
