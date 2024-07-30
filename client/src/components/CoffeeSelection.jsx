830c14167c7252131f03585323efb91270fcd9ee
arturterbaghdasarov1997 <133400776+arturterbaghdasarov1997@users.noreply.github.com>
2024-07-30 17:03:06 +0400

    Update CoffeeSelection.jsx

--- client/src/components/CoffeeSelection.jsx
+++ client/src/components/CoffeeSelection.jsx
@@ -1,32 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAdminCoffeeContext } from '../context/AdminCoffeeContext';
 
-const CoffeeSelection = ({ coffees = [], selectedCoffee, onSelectCoffee }) => {
-  if (!Array.isArray(coffees)) {
-    console.error('Expected coffees to be an array');
-    return null;
-  }
+function CoffeeForm() {
+    const { ingredients, submitCoffee, submitIngredient } = useAdminCoffeeContext();
 
-  return (
-    <div>
-      <h2>Select Your Favourite Coffee</h2>
-      <ul>
-        {coffees.map((coffee) => (
-          <li
-            key={coffee.id}
-            onClick={() => onSelectCoffee(coffee)}
-            style={{
-              cursor: 'pointer',
-              fontWeight: coffee.id === selectedCoffee?.id ? 'bold' : 'normal',
-              color: 'brown',
-            }}
-          >
-            <p>{coffee.title} - ₾{coffee.price}</p>
+    const [coffee, setCoffee] = useState({ title: "", description: "", price: "", ingredients: [] });
+    const [ingredient, setIngredient] = useState({ name: '', price: '', description: '' });
 
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    const handleCoffeeChange = (e) => {
+        setCoffee({ ...coffee, [e.target.name]: e.target.value });
+    };
 
-export default CoffeeSelection;
+    const handleIngredientChange = (e) => {
+        setIngredient({ ...ingredient, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await submitIngredient(ingredient);
+            await submitCoffee(coffee);
+            setCoffee({ title: "", description: "", price: "", ingredients: [] });
+            setIngredient({ name: '', price: '', description: '' });
+        } catch (error) {
+            console.error('Error creating coffee or ingredient:', error);
+        }
+    };
+
+    return (
+        <form
+            className='form'
+            onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                value={coffee.title}
+                onChange={handleCoffeeChange}
+                required
+            />
+            <input
+                type="text"
+                name="description"
+                placeholder="Description"
+                value={coffee.description}
+                onChange={handleCoffeeChange}
+                required
+            />
+            <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                value={coffee.price}
+                onChange={handleCoffeeChange}
+                required
+            />
+
+            <h2>Add New Ingredient</h2>
+            <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={ingredient.name}
+                onChange={handleIngredientChange}
+                required
+            />
+            <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                value={ingredient.price}
+                onChange={handleIngredientChange}
+                required
+            />
+
+            <button type="submit">Add Coffee and Ingredient</button>
+        </form>
+    );
+}
+
+export default CoffeeForm;
